f9f39766ac5ac258eccf1741437274375b0c7cbf
Coreytaz <coreytaz321@gmail.com>
2022-06-20 22:22:18 +0500

    4.4 Контекст This

---
+++ Часть 1/Глава 4 Обьекты/4.4/Использование this в литерале объекта.js
@@ -0,0 +1,14 @@
+// Здесь функция makeUser возвращает объект.
+
+// Каким будет результат при обращении к свойству объекта ref? Почему?
+
+// function makeUser() {
+//   return {
+//     name: "Джон",
+//     ref: this
+//   };
+// };
+
+// let user = makeUser();
+
+// alert( user.ref.name ); // Каким будет результат? // Будет ошибка can`t read property '' of undefined
