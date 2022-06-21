b2582803932a31af4209ba78ee1b55bba7be6743
Coreytaz <coreytaz321@gmail.com>
2022-06-21 21:56:47 +0500

    5.5 Методы массива

---
+++ Часть 1/Глава 5 Типы данных/5.5/Оставить уникальные элементы массива.js
@@ -0,0 +1,33 @@
+// Пусть arr – массив строк.
+
+// Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+// Например:
+
+// function unique(arr) {
+//   /* ваш код */
+// }
+
+// let strings = ["кришна", "кришна", "харе", "харе",
+//   "харе", "харе", "кришна", "кришна", ":-O"
+// ];
+
+// alert( unique(strings) ); // кришна, харе, :-O
+
+
+function unique(arr) {
+    let newarr = []
+    arr.forEach((element) => {
+        if (newarr.includes(element)) {
+            return;
+        }
+        newarr.push(element)
+    });
+    return newarr;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+    "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+console.log(unique(strings)); // кришна, харе, :-O
