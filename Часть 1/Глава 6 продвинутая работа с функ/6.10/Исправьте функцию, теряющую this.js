872f2df7c92c3768332d5dadf65e17f3b3b450c1
Coreytaz <coreytaz321@gmail.com>
2022-06-24 00:35:41 +0500

    6.10

---
+++ Часть 1/Глава 6 продвинутая работа с функ/6.10/Исправьте функцию, теряющую this.js
@@ -0,0 +1,26 @@
+// Ошибка происходит потому, что askPassword получает функции loginOk/loginFail без контекста.
+
+// Когда они вызываются, то, естественно, this=undefined.
+
+// Используем bind, чтобы передать в askPassword функции loginOk/loginFail с уже привязанным контекстом:
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: 'Вася',
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+
+};
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
