148a36ddd7caca6001467b44d9b1af769722e494
Coreytaz <coreytaz321@gmail.com>
2022-06-25 23:16:13 +0500

    1.7 Изменение документа

---
+++ Часть 2 Браузер документ, события, интерфейсы/Глава 1 Документ/1.7/createTextNode vs innerHTML vs textContent.js
@@ -0,0 +1,7 @@
+// У нас есть пустой DOM-элемент elem и строка text.
+
+// Какие из этих 3-х команд работают одинаково? // 1 and 3
+
+// elem.append(document.createTextNode(text))
+// elem.innerHTML = text
+// elem.textContent = text
